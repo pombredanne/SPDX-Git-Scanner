26b19140f3404dd6dc31d7d651ce5bd7e91747fc
Zachary Meyer <wif.temp@gmail.com>
2015-02-12 18:29:31 -0600

    Deleted redundant folder, added warning function if user adds bad dict key, revised formatting

--- DoSOCS/src/TEST-SCRIPT.py
+++ DoSOCS/src/TEST-SCRIPT.py
@@ -1,8 +1,14 @@
-import DoSPDX
+#import DoSPDX
+from dospdx_arghash import DospdxArgHash
+#argHash = { 'scanOption': 'fa', 
+#            'packagePath': '/home/zach/Downloads/fossology-spdx-master.zip',
+#            'scan': True,
+#            'documentComment': 'fs'}
 
-argHash = { 'scanOption': 'fa', 
-            'packagePath': '/home/zach/Downloads/fossology-spdx-master.zip',
-            'scan': True,
-            'documentComment': 'fs'}
+#DoSPDX.dospdx(argHash)
 
-DoSPDX.dospdx(argHash)
+good_test_dict = {'packagePath': '/'}
+bad_test_dict = {'dosga': 'sdgs'}
+
+DospdxArgHash(good_test_dict)
+DospdxArgHash(bad_test_dict)
--- DoSOCS/src/zdhash.py
+++ DoSOCS/src/zdhash.py
@@ -7,6 +7,9 @@ class ZDHash:
     def __init__(self, zdict):
         self.createDict()
         for key in zdict.keys():
-            self.dict[key] = zdict[key]
+            if key not in self.dict.keys():
+            	print "WARNING: " + key + " is not in the list.  Check your spelling"
+            
+            self.dict[key] = zdict[key]  
     def createDict(self):
         self.dict = {}
