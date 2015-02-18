56097c0668e9489d853e27c1ec4eb3ce451b82e7
Dan <dpatten@unomaha.edu>
2015-02-17 21:01:51 -0600

    Update install.sh
    Added set perms to setup

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Copyright Â© 2015 Daniel Patten, Zachary Meyer, and Jacob Vosik
+#Copyright © 2015 Daniel Patten, Zachary Meyer, and Jacob Vosik
 #Setup User Name and Password for MySql
 read -p "Enter your MySQL Username:" u
 stty -echo 
-read -p "Enter your MySQL Password: " p; echo 
-stty echo
+ read -p "Enter your MySQL Password: " p; echo 
+ stty echo
 #Clone Repos
 git clone https://github.com/socs-dev-env/SOCSDatabase
 
@@ -16,5 +16,6 @@ mysql --user=$u --password=$p < SOCSDatabase/SQL/SPDX.sql
 #Delete Database Repo
 echo "Remove Database Repo"
 sudo rm SOCSDatabase -R
-
+chmod 755 src/*.py
+chmod 755 DoSOCS/src/*.py
 echo "Install Complete"
