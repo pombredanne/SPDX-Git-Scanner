5a76bf16950c0f471c6abb64464bb8a91c6e19e6
Dan <dpatten@unomaha.edu>
2015-02-17 20:18:16 -0600

    Initial commit of install script, code cleanup
    Removed SOCSDatabase as it works from Git clone. Added initial
    install.sh script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Copyright © 2015 Daniel Patten, Zachary Meyer, and Jacob Vosik
+#Setup User Name and Password for MySql
+read -p "Enter your MySQL Username:" u
+stty -echo 
+ read -p "Enter your MySQL Password: " p; echo 
+ stty echo
+#Clone Repos
+git clone https://github.com/socs-dev-env/SOCSDatabase
+
+#Install Database
+echo "Install SPDX Database..."
+mysql --user=$u --password=$p < SOCSDatabase/SQL/SPDX.sql
+#Exit mySql
+
+#Delete Database Repo
+echo "Remove Database Repo"
+sudo rm SOCSDatabase -R
+
+echo "Install Complete"
