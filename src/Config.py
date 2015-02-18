4aae5c3005a9eed95ce3b6dc5993ec02878f31e3
Jacob Vosik <jacob.vosik@gmail.com>
2015-02-17 19:30:38 -0600

    Configuration parser and file

---
+++ src/Config.py
@@ -0,0 +1,112 @@
+import os
+
+class Config:
+    def __init__( self ):
+        self._params = {}
+        self.Set( 'Branch'          , None                      )
+        #self.Set( 'SPDXVers'        , 2.0                       )
+        self.Set( 'TmpDir'          , os.path.join('.','TMP')   )
+        self.Set( 'Verbose'         , True                      )
+        
+
+    def Parse( self, paramList ):
+        for param in paramList:
+            if not '=' in param:
+                name, value = param,""
+            else:
+                name, value = param.split('=',1)
+            self.Set( name, value )
+
+    def ParseFile( self, fileName ):
+        try:
+            file = open( fileName, 'r' )
+            lines = file.readlines()
+            file.close()
+
+            vParseLines = []
+            for line in lines:
+                if not line.strip().startswith('//'):
+                    vParseLines.append( line )
+
+            self.Parse( vParseLines )
+        except:
+            pass
+
+    def HasParm( self, param ):
+        return self.Get( param ) != None
+
+    def Get( self, param ):
+        param = param.lower()
+        if param in self._params.keys():
+            return self._params[ param ]
+        return None
+
+    def GetAsNum( self, param ):
+        value = self.Get(param)
+        try:
+            if '.' in value:
+                return float(value)
+            else:
+                return int(value)
+        except:
+            raise Exception( "Expected value to be a number, but there was an error converting it: [" + value + "]" )
+
+    def GetAsBool( self, param ):
+        value = self.Get(param)
+        return value != None and value.lower() == 'true'
+        
+    def Set( self, param, value ):
+        param = param.strip().lower()
+
+        if not len(param):
+            return
+
+        if value == None:
+            value = ''
+        elif not type(value) == str:
+            value = repr(value)
+            
+        value = value.strip()
+        self._params[ param ] = value
+
+    def PrintConfig( self ):
+        longest = 0
+        for key in self._params.keys():
+            if len(key) > longest:
+                longest = len(key)
+        fmt = "%-" + repr(longest) + "s = %s"
+        for key in self._params.keys():
+            print( fmt % (key, self._params[key]) )
+
+def __TestConfig():
+    c = Config()
+    firstList = """
+    Hello=blah
+    NoVal=
+    BAD_LINE
+    one=1
+    two = 2
+    =
+    flag
+    """.split("\n")
+
+    secondList = """
+    Hello=World
+    """.split("\n")
+
+    c.Parse( firstList )
+    c.PrintConfig()
+    c.Parse( secondList )
+    c.PrintConfig()
+
+    c.Set( 'two', 2.0 )
+    print( c.Get( 'two' ) )
+
+    print( c.GetAsNum('onE') )
+    try:
+        c.GetAsNum('Hello')
+    except Exception as e:
+        print( e )
+
+if __name__ == '__main__':
+    __TestConfig()
