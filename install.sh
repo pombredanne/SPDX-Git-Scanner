9b834b8f632dd16078aecbd8068ae3df7376da54
DanPatten <DanPatten@users.noreply.github.com>
2015-03-12 19:09:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ fi
 
 #Delete Database Repo
 echo "Setting up files..."
-cp -r DoSOCS SPDX-Git-Scanner
+cp -r DoSOCS SPDX-Git-Scanner/src
 chmod 755 SPDX-Git-Scanner/src/*.py
 chmod 755 SPDX-Git-Scanner/DoSOCS/src/*.py
 sudo rm DoSOCS -R
