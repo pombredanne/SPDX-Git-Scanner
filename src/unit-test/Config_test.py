45c4daf3530859ab0c6eb10b4a13970bdd49e375
Zachary Meyer <wif.temp@gmail.com>
2015-03-17 19:11:09 -0500

    Initial Config Test Suite

---
+++ src/unit-test/Config_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class ConfigTestSuite(unitTest.TestSuite):
+	class cfgMthdParseTestSuite(unittest.TestSuite):
+	class cfgMthdParseFileTestSuite(unittest.TestSuite):
+	class cfgMthdHasParmTestSuite(unittest.TestSuite):
+	class cfgMthdGetTestSuite(unittest.TestSuite):
+	class CfgMthdGetAsNumTestSuite(unittest.TestSuite):
+	class CfgMthdGetAsBoolTestSuite(unittest.TestSuite):
+	class cfgMthdSetTestSuite(unittest.TestSuite):
+	class cfgMthdPrintConfigTestSuite(unittest.TestSuite):
