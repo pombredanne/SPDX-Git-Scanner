3a4deadb1bbfbfe0c0bc5efc0a598aa0d8ea8b73
Zachary Meyer <wif.temp@gmail.com>
2015-03-30 16:57:42 -0500

    Can now execute runUnitTest from any directory

--- src/runUnitTest.py
+++ src/runUnitTest.py
@@ -1,6 +1,9 @@
 import sys
-# TODO add execution directory in order to execute this from anywhere.
-sys.path.append("unit-test")
+import os
+if os.name == "nt":
+	sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "\unit-test")
+else:
+	sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/unit-test")	
 
 import runTest
 
